80ca0e0088de235c97c7914f52f4e256412c223e
devin jhu <devin.jhu@mths.ca>
2022-11-01 03:01:33 +0000

    code

---
+++ MrCoxallStack.java
@@ -0,0 +1,49 @@
+/*
+* The MrCoxallStack.
+*
+* @author  Devin Jhu
+* @version 1.0
+* @since   2022-10-31
+*/
+
+import java.util.ArrayList;
+/**
+*  Make class MrCoxallStack.
+*/
+
+public class MrCoxallStack {
+
+    /**
+    * An ArrayList that will hold the tack elements.
+    */
+    private ArrayList<Integer> theStack = new ArrayList<Integer>();
+
+    /**
+    * Getter.
+    * The showStack method.
+    *
+    * @return regets stack and returns it
+    */
+    public ArrayList<Integer> getStack() {
+        return theStack;
+    }
+
+    /**
+    * The push() function.
+    *
+    * @param pushNumber The int to be added to the stack
+    */
+    public void push(final int pushNumber) {
+        final int index = theStack.size();
+        theStack.add(index, pushNumber);
+    }
+    /**
+    * The pop() function.
+    *
+    * @return Remove the top element and return it
+    */
+
+    public int pop() {
+        return theStack.remove(theStack.size() - 1);
+    }
+}
